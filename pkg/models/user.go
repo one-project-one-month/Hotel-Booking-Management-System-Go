b527b16ba3cfa8275a503198672146ff3c718688
Kaung Min Htet <kgminhtet.dev@gmail.com>
2025-05-21 09:23:34 +0700

    add conpons to user model

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -34,4 +34,5 @@ type User struct {
 	CreatedAt   time.Time      `gorm:"autoCreateTime"`
 	UpdatedAt   time.Time      `gorm:"autoUpdateTime"`
 	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	Coupons     []Coupon
 }
