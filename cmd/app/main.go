fde26a079397886a4920fd386d945ed5b31183d7
Kaung Min Htet <kgminhtet.dev@gmail.com>
2025-05-27 17:46:13 +0630

    feat(bank-account): initialize bank account service

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/one-project-one-month/Hotel-Booking-Management-System-Go/config"
+	"github.com/one-project-one-month/Hotel-Booking-Management-System-Go/internal/bankaccount"
 	"github.com/one-project-one-month/Hotel-Booking-Management-System-Go/internal/booking"
 	"github.com/one-project-one-month/Hotel-Booking-Management-System-Go/internal/checkinout"
 	"github.com/one-project-one-month/Hotel-Booking-Management-System-Go/internal/coupon"
@@ -37,6 +38,7 @@ func main() {
 	coupon.Run(app.echo, db, queue)
 	booking.Run(app.echo, db, queue)
 	checkinout.Run(app.echo, db, queue)
+	bankaccount.Run(app.echo, db, queue)
 
 	app.echo.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "Welcome to Hotel Booking System APIs")
